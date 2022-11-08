9bfdef8fa7730f755b2385936bdd817ee4b09cdb
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-11-08 01:41:25 -0500

    Updated Header with more og tags

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,13 +9,15 @@ const Main = ({ children }) => {
             <Head>
                 <title>plnguyen</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="Patrick-Lam Nguyen's homepage. Full Stack Wev Developer based in Montreal." />
+                <meta name="description" content="Patrick-Lam Nguyen's homepage." />
                 <meta name="author" content="Patrick-Lam Nguyen" />
                 <link rel="icon" href="/coding.ico" />
+                <meta name="og:site_title" content="plnguyen.dev" />
                 <meta property="og:site_name" content="plnguyen" />
-                <meta name="og:site_title" content="Patrick-Lam Nguyen's homepage" />
+                <meta property="og:url" content="https://plnguyen.dev" />
+                <meta property="og:image" content="https://res.cloudinary.com/ditoikfqn/image/upload/v1667889281/NextJS-Portfolio/og_image_saxpip.jpg" />
                 <meta property="og:type" content="website" />
-                <meta property="og:image" content="https://res.cloudinary.com/ditoikfqn/image/upload/v1667780360/NextJS-Portfolio/homepage-v3_ju7vrn.jpg" />
+                <meta property="og:description" content="Patrick-Lam Nguyen's homepage. Full Stack Wev Developer based in Montreal." />
             </Head>
 
             <Navbar />
